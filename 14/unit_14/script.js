dfa92a69283255ff61e12cd05394d417bc7911e0
SerhiyAndryeyev <s.andryeyev@gmail.com>
2021-12-22 17:40:30 +0200

    add finish changes to app

--- 14/unit_14/script.js
+++ 14/unit_14/script.js
@@ -1,15 +1,34 @@
-// create select' list
+//
+// достать текст из выбраного оптион из селекта:
+// let index = ваш_select.selectedIndex;
+// let text = ваш_select.options[index].text
+//
+// create select' list:
 
-// html = "";
-// const cities = {
-//   23232: "london",
-//   33456: "minsk",
-//   //и так далее.
-// };
-// for (var key in obj) {
-//   html += "<option value=" + key + ">" + obj[key] + "</option>";
-// }
-// document.getElementById("cities").innerHTML = html;
+let options = ``;
+const cities = {
+  703448: "Kyiv",
+  702550: "Lviv",
+  2643743: "London",
+  6269531: "Paris",
+  756135: "Warsaw",
+  687116: "Zhmerynka",
+  686967: "Zhytomyr",
+  616051: "Yerevan",
+  3081368: "Wrocław",
+  2998324: "Lille",
+  5128638: "New York",
+  4887398: "Chicago",
+  4164138: "Miami",
+  6167865: "Toronto",
+  2800866: "Brussels",
+};
+
+for (let key in cities) {
+  options += `<option value="${key}">${cities[key]}</option>`;
+}
+
+document.getElementById("cities").innerHTML = options;
 
 const param = {
   "url": "https://api.openweathermap.org/data/2.5/",
@@ -29,7 +48,13 @@ function showWeather(data) {
   const temperature = `🌡 ${Math.round(data.main.temp)}&deg;`;
   const humidity = `humidity is ${data.main.humidity}&#37;`;
   const pressure = `pressure is ${data.main.pressure}mbar`;
+  const iconId = data.weather[0].icon;
+  console.log('iconId= ', iconId);
+
+  document.querySelector('.city-choise-show').innerHTML = `The weather in ${data.name}:`;
   document.querySelector('.temperature-show').innerHTML = temperature;
+  document.querySelector('.icon-show').innerHTML = `<img src="http://openweathermap.org/img/wn/${iconId}@2x.png" alt="weather icon">`;
+  document.querySelector('.weather-description').textContent = `${data.weather[0].description}`;
   document.querySelector('.weather-out').innerHTML = `${humidity}<br>${pressure}`;
 
 }
